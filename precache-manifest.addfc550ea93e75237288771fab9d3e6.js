90f7f6dceae49db1e529b4d8c75112fa3d1e15dc
Doug Bainbridge <Doug@Bainbridges-MacBook-Pro.local>
2019-06-27 20:43:22 +0100

    Updates

---
+++ precache-manifest.addfc550ea93e75237288771fab9d3e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97617602b06e8a4b08fada8d5ede0021",
+    "url": "/emmanuel-song-pool/index.html"
+  },
+  {
+    "revision": "f735ded65bbadf73fdbb",
+    "url": "/emmanuel-song-pool/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5b60c6ce7c94c2302f8c",
+    "url": "/emmanuel-song-pool/static/css/main.7e86e227.chunk.css"
+  },
+  {
+    "revision": "f735ded65bbadf73fdbb",
+    "url": "/emmanuel-song-pool/static/js/2.a825ad85.chunk.js"
+  },
+  {
+    "revision": "5b60c6ce7c94c2302f8c",
+    "url": "/emmanuel-song-pool/static/js/main.88a3c44e.chunk.js"
+  },
+  {
+    "revision": "a231624ed706ae1682ac",
+    "url": "/emmanuel-song-pool/static/js/runtime~main.c59a8203.js"
+  }
+]);
