53de68f930f7f90beaa5aed16c0cbac2e92f22ee
Doug Bainbridge <Doug@Bainbridges-MacBook-Pro.local>
2019-06-29 00:47:05 +0100

    more categories

--- src/views/PrePreach.js
+++ src/views/PrePreach.js
@@ -8,10 +8,21 @@ export default function PrePreach(props) {
     const callToWorshipDeclaration = filterSongBySubCat(props.songs, 'Call To Worship', 'Declaration & Praise', props.focusSongs)
     const callToWorshipDraw = filterSongBySubCat(props.songs, 'Call To Worship', 'Drawing Near', props.focusSongs)
     const revelationName = filterSongBySubCat(props.songs, 'Revelation', 'Name of Jesus', props.focusSongs)
-    const revelationAssurance = filterSongBySubCat(props.songs, 'Revelation', 'Assurance in Christ', props.focusSongs)
     const revelationSufficiency = filterSongBySubCat(props.songs, 'Revelation', 'Sufficiency of Christ', props.focusSongs)
     const revelationStory = filterSongBySubCat(props.songs, 'Revelation', 'Gospel Story', props.focusSongs)
     const revelationAdoption = filterSongBySubCat(props.songs, 'Revelation', 'Adoption in Christ', props.focusSongs)
+    const revelationAscended = filterSongBySubCat(props.songs, 'Revelation', 'Ascended Christ', props.focusSongs)
+    const revelationAssurance = filterSongBySubCat(props.songs, 'Revelation', 'Assurance in Christ', props.focusSongs)
+    const revelationEternity = filterSongBySubCat(props.songs, 'Revelation', 'Eternity', props.focusSongs)
+    const revelationLove = filterSongBySubCat(props.songs, 'Revelation', 'Love of God', props.focusSongs)
+    const revelationGoodness = filterSongBySubCat(props.songs, 'Revelation', 'Goodness of God', props.focusSongs)
+    const revelationGreatness = filterSongBySubCat(props.songs, 'Revelation', 'Greatness of God', props.focusSongs)
+    const revelationFaithfulness = filterSongBySubCat(props.songs, 'Revelation', 'Faithfulness of God', props.focusSongs)
+    const revelationHoliness = filterSongBySubCat(props.songs, 'Revelation', 'Holiness of God', props.focusSongs)
+    const revelationSuffering = filterSongBySubCat(props.songs, 'Revelation', 'God in Suffering', props.focusSongs)
+    const responseAdoration = filterSongBySubCat(props.songs, 'Response', 'Adoration', props.focusSongs)
+
+
     return (
         <Carousel>
             <ListColumn title="Call to Worship">
@@ -23,7 +34,24 @@ export default function PrePreach(props) {
                 <List title="Gospel Story" list={revelationStory} />
                 <List title="Sufficiency of Christ" list={revelationSufficiency} />
                 <List title="Adoption in Christ" list={revelationAdoption} />
+                <List title="Ascended Christ" list={revelationAscended} />
                 <List title="Assurance in Christ" list={revelationAssurance} />
+                <List title="Eternity" list={revelationEternity} />
+                <List title="Love of God" list={revelationLove} />
+                <List title="Goodness of God" list={revelationGoodness} />
+                <List title="Greatness of God" list={revelationGreatness} />
+                <List title="Faithfulness of God" list={revelationFaithfulness} />
+                <List title="Holiness of God" list={revelationHoliness} />
+                <List title="God in Suffering" list={revelationSuffering} />
+            </ListColumn>
+            <ListColumn title="Response">
+                <List title="Adoration" list={responseAdoration} />
+            </ListColumn>
+            <ListColumn title="Preach">
+            </ListColumn>
+            <ListColumn title="Communion">
+            </ListColumn>
+            <ListColumn title="Revelation/Response/Sending">
             </ListColumn>
         </Carousel>
     )
