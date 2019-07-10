bf9a8eb92ade5b4562faa0e1dac34c652a806e66
Doug Bainbridge <dougjbainbridge@gmail.com>
2019-07-10 11:04:03 +0100

    colors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,15 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div
+        <a
+            href="mailto:worship@weareemmanuel.com?subject=Song Pool Feedback&body=Hi there, please don't change the subject line of this email as it will get auto forwarded."
             style={{
                 position: 'fixed',
                 bottom: 40,
                 right: 20,
-                background: 'white',
-                borderRadius: '32px',
-                padding: '0px 12px',
-                borderColor: '#EDC331',
-                borderWidth: '5px',
-                borderStyle: 'solid'
-            }} className="shadow">
-            <a href="mailto:worship@weareemmanuel.com?subject=Song Pool Feedback&body=Hi there, please don't change the subject line this email will get auto forwarded." className="btn-link mb-0" >
-                feedback
+            }} className="shadow contact-btn">
+            feedback
         </a>
-        </div>
 
     )
 }
