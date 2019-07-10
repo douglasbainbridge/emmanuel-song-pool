cfd3c690ff9e5870996ee895788ac5758c2b4d7b
Doug Bainbridge <dougjbainbridge@gmail.com>
2019-07-10 11:14:02 +0100

    Updates

--- precache-manifest.40d17fb9bc4889e55ce268ca873fa120.js
+++ precache-manifest.40d17fb9bc4889e55ce268ca873fa120.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09d1f1f09d23e990a2cf18c48a895d49",
+    "revision": "e3e564bdff5219b66457a6fd03dc354f",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.b6b2d2da.chunk.css"
   },
   {
-    "revision": "83afdaa361d57abad916",
+    "revision": "cf3f1b748e9b0eed17f4",
     "url": "/static/css/main.747dbdb2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.caeed97f.chunk.js"
   },
   {
-    "revision": "83afdaa361d57abad916",
-    "url": "/static/js/main.3148b116.chunk.js"
+    "revision": "cf3f1b748e9b0eed17f4",
+    "url": "/static/js/main.d67eb3ee.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4feddc05f87d2694fa3f256882969ccf.js"
+  "/precache-manifest.40d17fb9bc4889e55ce268ca873fa120.js"
 );
 
 self.addEventListener('message', (event) => {
